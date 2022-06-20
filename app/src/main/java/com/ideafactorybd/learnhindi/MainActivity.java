55c7bd0205cd87e87d9d0756e3fb3c283f427f7e
mknishad <mknishad1@ymail.com>
2022-06-20 22:54:27 +0600

    implement banner ad

--- app/src/main/java/com/ideafactorybd/learnhindi/MainActivity.java
+++ app/src/main/java/com/ideafactorybd/learnhindi/MainActivity.java
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,5 +37,11 @@ public class MainActivity extends AppCompatActivity {
     // Give the TabLayout the ViewPager
     TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
     tabLayout.setupWithViewPager(viewPager);
+
+    // Admob
+    MobileAds.initialize(this);
+    AdView mAdView = findViewById(R.id.adView);
+    AdRequest adRequest = new AdRequest.Builder().build();
+    mAdView.loadAd(adRequest);
   }
 }
